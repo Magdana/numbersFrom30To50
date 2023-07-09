a67f344dc382b7ee951a491baf2ee2b7291d810e
Magda <magdagvirjishvili@gmail.com>
2023-07-10 00:42:01 +0400

    magdas_commit

---
+++ numbersFrom30To50/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("numbers from 30 to 50");
+       for (int i = 30; i <=50;  i++) 
+        { 
+            Console.WriteLine(i);
+        }
+    }
+}
